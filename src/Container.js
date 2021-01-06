a85444d2dc3621e466aecc24e5702580d7dbf674
Ada <serafino.ada@gmail.com>
2021-01-06 16:03:30 +0100

    Added form, button and cards

--- src/Container.js
+++ src/Container.js
@@ -1,6 +1,40 @@
 import React from "react";
+import Form from "./Form.js"
+import "./Container.css"
+import Geolocate from "./Geolocate"
+import Current from "./Current"
 
 export default function Container() { return (
-<div> f</div>
+
+
+<div class="container">   
+
+
+
+<div className ="container-app">
+<h1>FORECAST</h1>
+
+<h2>DATE</h2>
+
+<div class="row">
+<div class="col-7">
+    <div class="row"> <Form/> </div>
+    <div class="row"><Geolocate/></div>
+</div>
+
+
+<div class="col-5">
+<Current />
+</div>
+
+<div class="row">forecast</div>
+
+</div>
+
+</div>
+<div className="extra">
+    <footer>hi</footer>
+</div>
+</div>
 )
 }
---
+++ src/Geolocate.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Geolocate.css";
+
+export default function Geolocate() {
+  return (
+      <div className ="card">
+          <div className ="card-body">
+    <button>
+      {" "}
+      current <i class="fas fa-map-marked-alt"></i>
+    </button>
+    </div>
+    </div>
+  );
+}
